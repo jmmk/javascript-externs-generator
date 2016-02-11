89b32e6c3e531cd5cf3a1ad3d08f3ea4e0b9e5cc
Michael McLellan <jmikem825@gmail.com>
2016-02-10 22:32:28 -0500

    Add docstrings to extern namespace

--- src/javascript_externs_generator/extern.cljs
+++ src/javascript_externs_generator/extern.cljs
@@ -1,16 +1,23 @@
 (ns javascript-externs-generator.extern
   (:require [clojure.string :as string]))
 
-(defn get-type [obj]
+(defn get-type
+  "Differentiate between function property and object property"
+  [obj]
   (if (fn? obj)
     :function
     :object))
 
-(defn parent-type? [obj]
+(defn parent-type?
+  "Whether it is possible for this object to have child properties"
+  [obj]
   (or (object? obj)
       (fn? obj)))
 
-(defn build-tree [obj name]
+(defn build-tree
+  "Build a recursive tree representation of the JavaScript object
+  and metadata about its properties, prototype, and functions"
+  [obj name]
   {:name name
    :type (get-type obj)
    :props (for [k (js-keys obj)
@@ -22,13 +29,19 @@
    :prototype (for [k (js-keys (.-prototype obj))]
                 {:name k :type :function})})
 
-(defn build-function [name]
+(defn build-function
+  "Return string representation of a function"
+  [name]
   (str (quote-string name) ": function () {}"))
 
-(defn build-object [name]
+(defn build-object
+  "Return string representation of an object property"
+  [name]
   (str (quote-string name) ": {}"))
 
-(defn build-props-extern [obj]
+(defn build-props-extern
+  "Return recursive string representation of an object's properties"
+  [obj]
   (let [{:keys [name type props]} obj]
     (cond
       (and (empty? props)
@@ -46,19 +59,26 @@
           (str "var " name "=" props-extern ";")
           (str (quote-string name) ":" props-extern))))))
 
-(defn build-prototype [namespace prototype]
+(defn build-prototype
+  "Return string representation of an object's prototype/functions"
+  [namespace prototype]
   (when (seq prototype)
     (str namespace ".prototype = {" (string/join "," (for [p prototype]
                                                        (build-function (:name p)))) "};")))
 
-(defn build-prototype-extern [obj namespace]
+(defn build-prototype-extern
+  "Return recursive string representation of an object's prototype chain"
+  [obj namespace]
   (let [{:keys [name props prototype]} obj
         prototype-extern (build-prototype namespace prototype)
         child-prototype-externs (for [p props]
                                   (build-prototype-extern p (str namespace "." (:name p))))]
     (str prototype-extern (string/join child-prototype-externs))))
 
-(defn extract [name]
+(defn extract
+  "Given the name of a JavaScript object that has been loaded into the global namespace,
+  recursively extract properties and prototypes into an extern for use with ClojureScript"
+  [name]
   (let [js-object (aget js/window name)
         tree (assoc (build-tree js-object name) :top true)
         props-extern (build-props-extern tree)
