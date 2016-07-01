1a4cbaec050ff6b5e9805d8f6e630631c114745b
Michael McLellan <jmikem825@gmail.com>
2016-07-01 19:34:16 -0400

    Improve function and variable names

--- src/javascript_externs_generator/extern.cljs
+++ src/javascript_externs_generator/extern.cljs
@@ -3,7 +3,7 @@
             [goog.object :as obj]
             [clojure.string :as string]))
 
-(defn get-type
+(defn prop-type
   "Differentiate between function property and object property"
   [obj]
   (if (fn? obj)
@@ -16,57 +16,57 @@
   (or (= (goog/typeOf obj) "object")
       (fn? obj)))
 
-(defn build-tree
+(defn generate-object-tree
   "Build a recursive tree representation of the JavaScript object
   and metadata about its properties, prototype, and functions"
   [obj name]
   {:name      name
-   :type      (get-type obj)
-   :props     (for [k (js-keys obj)
-                    :let [child (obj/get obj k)]]
+   :type      (prop-type obj)
+   :props     (for [prop-name (js-keys obj)
+                    :let [child (obj/get obj prop-name)]]
                 (if (and (parent-type? child)
                          (not (identical? obj child)))
-                  (build-tree child k)
-                  {:name k :type (get-type child)}))
-   :prototype (for [k (js-keys (.-prototype obj))]
-                {:name k :type :function})})
+                  (generate-object-tree child prop-name)
+                  {:name prop-name :type (prop-type child)}))
+   :prototype (for [prop-name (js-keys (.-prototype obj))]
+                {:name prop-name :type :function})})
 
-(defn build-props-extern
+(defn emit-props-extern
   "Return recursive string representation of an object's properties"
   [obj]
   (let [{:keys [name type props]} obj
         function-str (if (and (= type :function)
                               (empty? props)) "function()" "")
         props-extern (str "{" (string/join "," (for [p props]
-                                                 (build-props-extern p))) "}")]
+                                                 (emit-props-extern p))) "}")]
     (if (get obj :root)
       (str "var " name " = " function-str props-extern ";")
       (str (quote-string name) ": " function-str props-extern))))
 
 
-(defn build-prototype
+(defn emit-prototype
   "Return string representation of an object's prototype/functions"
   [namespace prototype]
   (when (seq prototype)
     (str namespace ".prototype = {" (string/join "," (for [p prototype]
                                                        (str (quote-string (:name p)) ": function(){}"))) "};")))
 
-(defn build-prototype-extern
+(defn emit-prototype-extern
   "Return recursive string representation of an object's prototype chain"
   [obj namespace]
   (let [{:keys [name props prototype]} obj
-        prototype-extern (build-prototype namespace prototype)
+        prototype-extern (emit-prototype namespace prototype)
         child-prototype-externs (for [p props]
-                                  (build-prototype-extern p (str namespace "." (:name p))))]
+                                  (emit-prototype-extern p (str namespace "." (:name p))))]
     (str prototype-extern (string/join child-prototype-externs))))
 
 (defn extract
   "Recursively extract properties and prototypes from a JavaScript object
   into an extern for use with the Google Closure Compiler"
   [name js-object]
-  (let [tree (assoc (build-tree js-object name) :root true)
-        props-extern (build-props-extern tree)
-        prototype-extern (build-prototype-extern tree name)]
+  (let [tree (assoc (generate-object-tree js-object name) :root true)
+        props-extern (emit-props-extern tree)
+        prototype-extern (emit-prototype-extern tree name)]
     (str props-extern prototype-extern)))
 
 (defn extract-loaded
